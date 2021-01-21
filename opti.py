d38ac1b47be935e0bca331ed4b5716c3112e12bb
codingCup04 <68053011+codingCup04@users.noreply.github.com>
2021-01-21 18:46:35 +0900

    Update opti.py

--- opti.py
+++ opti.py
@@ -56,6 +56,35 @@ def add(val1, val2):
     return val1 + val2
 def pi():
     return math.pi
+def pycaptcha():
+    import tkinter as tk
+import random
+requ = random.randrange(0, 11)
+pressed = 0
+closed = False
+print(f"Click the button {requ} times.")
+def printout():
+    global pressed
+    pressed += 1
+    print(f"Pressed {pressed} times.")
+def closewin():
+    print("Verifications window closed.")
+    closed = True
+window = tk.Tk()
+window.title("Test")
+window.wm_iconbitmap('appicon.ico')
+window.geometry("500x200")
+button = tk.Button(window, activebackground="#000000", activeforeground="#ffffff", text="Click here", command=printout)
+button.place(x=100, y=10)
+info1 = tk.Label(text="pyCaptcha")
+info1.place(x=10, y=10)
+window.mainloop()
+if requ == pressed:
+    print("Verification complete.")
+    return True
+else:
+    print("Verification failed.")
+    return False
 
 class printOut:
     def __init__():
